e276f8c955715367d6a446ca0e87b671c0676a0e
james <james@jamess-MacBook-Pro.local>
2020-06-10 13:24:07 -0700

    step 6 done

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, EditUserForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -215,6 +215,33 @@ def profile():
     """Update profile for current user."""
 
     # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    form = EditUserForm(obj=g.user)
+
+    if form.validate_on_submit():
+        if User.authenticate(g.user.username,
+                            form.password.data):
+
+            g.user.username = form.username.data
+            g.user.email = form.email.data
+            g.user.image_url = form.image_url.data
+            g.user.bio = form.bio.data
+            g.user.header_image_url = form.header_image_url.data
+
+            db.session.commit()
+
+            return redirect(f'/users/{g.user.id}')
+
+        else: 
+            flash("Your password is wrong, please try again")
+            return redirect('/')
+
+    return render_template('/users/edit.html', form=form)
+    
+
 
 
 @app.route('/users/delete', methods=["POST"])
@@ -295,11 +322,14 @@ def homepage():
     """
 
     if g.user:
+        following_messages = [message.id for message in g.user.following]
+        following_messages.append(g.user.id)
         messages = (Message
-                    .query
-                    .order_by(Message.timestamp.desc())
-                    .limit(100)
-                    .all())
+                .query
+                .filter(Message.user_id.in_(following_messages))
+                .order_by(Message.timestamp.desc())
+                .limit(100)
+                .all())
 
         return render_template('home.html', messages=messages)
 
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, Optional
 
 
 class MessageForm(FlaskForm):
@@ -23,3 +23,13 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class EditUserForm(FlaskForm):
+    """Edit user form"""
+
+    username = StringField('Username')
+    email = StringField('E-mail', validators=[Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+    bio = StringField("Bio")
+    header_image_url = StringField("Header Image")
